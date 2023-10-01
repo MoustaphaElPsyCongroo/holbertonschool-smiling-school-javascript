c3021b8d5b996a3928cab142927a9a25bd90f4e3
MousElPsyCongroo <mtapha.tall@gmail.com>
2023-10-01 23:29:11 +0200

    2 - Try making carousel cards slide one by one

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 const $ = window.$;
 
 $(function () {
-  function loadCarousel () {
+  function loadQuotes () {
     const carousel = $('#carouselExampleControls');
-    const carouselInner = $('.carousel-inner');
-    const loadingSpinner = $('.loader');
+    const carouselInner = $('#carouselExampleControls .carousel-inner');
+    const loadingSpinner = $('#carouselExampleControls .loader');
 
     carousel.children().hide();
-    carousel.append(loadingSpinner);
+    loadingSpinner.show();
 
     $.get('https://smileschool-api.hbtn.info/quotes', function (res, status, jqxhr) {
       if (status === 'success' && jqxhr.status === 200) {
@@ -45,5 +45,99 @@ $(function () {
     });
   }
 
-  loadCarousel();
+  function loadVideos () {
+    const carousel = $('#carouselExampleControls2');
+    const loadingSpinner = $('#carouselExampleControls2 .loader');
+
+    carousel.children().hide();
+    loadingSpinner.show();
+
+    $.get('https://smileschool-api.hbtn.info/popular-tutorials', function (res, status, jqxhr) {
+      if (status === 'success' && jqxhr.status === 200) {
+        carousel.children().show();
+        loadingSpinner.hide();
+
+        let i = 0;
+        for (const video of res) {
+          let stars = '';
+
+          for (let j = 0; j < video.star; j++) {
+            stars += `<img
+              src="images/star_on.png"
+              alt="star on"
+              width="15px"
+            />`;
+          }
+
+          const carouselCard = `<div class="carousel-item ${i === 0 ? 'active' : ''}">
+          <div
+            class="col-lg-3 col-md6 col-sm-6 col-12 d-flex justify-content-center"
+          >
+            <div class="card">
+              <img
+                src="${video.thumb_url}"
+                class="card-img-top"
+                alt="Video thumbnail"
+              />
+              <div class="card-img-overlay text-center">
+                <img
+                  src="images/play.png"
+                  alt="Play"
+                  width="64px"
+                  class="align-self-center play-overlay"
+                />
+              </div>
+              <div class="card-body">
+                <h5 class="card-title font-weight-bold">
+                  ${video.title}
+                </h5>
+                <p class="card-text text-muted">
+                  ${video['sub-title']}
+                </p>
+                <div class="creator d-flex align-items-center">
+                  <img
+                    src="${video.author_pic_url}"
+                    alt="Creator of
+                    Video"
+                    width="30px"
+                    class="rounded-circle"
+                  />
+                  <h6 class="pl-3 m-0 main-color">${video.author}</h6>
+                </div>
+                <div class="info pt-3 d-flex justify-content-between">
+                  <div class="rating">${stars}</div>
+                  <span class="main-color">${video.duration}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>`;
+
+          $('#carouselExampleControls2 .carousel-inner .row').append(carouselCard);
+          i++;
+        }
+
+        $('#carouselExampleControls2 .carousel-item').each(function () {
+          const minPerSlide = 4;
+          let next = $(this).next();
+          if (!next.length) {
+            next = $(this).siblings(':first');
+          }
+          next.children(':first-child').clone().appendTo($(this));
+
+          for (let i = 0; i < minPerSlide; i++) {
+            next = next.next();
+            if (!next.length) {
+              next = $(this).siblings(':first');
+            }
+
+            next.children(':first-child').clone().appendTo($(this));
+          }
+        });
+      }
+    });
+  }
+
+  loadQuotes();
+  loadVideos();
 });
